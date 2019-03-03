7bba2282951e482911c8d8891f751ce7d9faf6b9
phamxuanhoang <hoanganpha@gmail.com>
2019-03-03 23:42:12 +0700

    first commit

---
+++ src/laptrinhjava/LaptrinhJava.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laptrinhjava;
+
+/**
+ *
+ * @author PHAM XUAN HOANG
+ */
+public class LaptrinhJava {
+   
+    public static void main(String[] args) {
+        System.out.println("HOàng");
+    }
+
+    
+    
+}
